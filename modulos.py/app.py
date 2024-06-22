40ba77a7531a16b4c1ae28b83b37817565f7883b
jayounghoyos <juanandresyounghoyos@gmail.com>
2024-06-21 21:23:01 -0500

    dir

--- modulos.py/app.py
+++ modulos.py/app.py
@@ -1,4 +1,16 @@
-from usuarios.acciones import guardar
-from usuarios import acciones
+#from usuarios.acciones import guardar
+#from usuarios import acciones
 
-acciones.guardar()
+#from usuarios.acciones.utilidades import guardar
+
+from usuarios.impuestos.utilidades import pagar_impuestos
+import usuarios
+
+print(usuarios.__package__)
+print("*"*10)
+
+print(usuarios.__name__)
+print("*"*10)
+print(usuarios.__path__)
+print("*"*10)
+print(usuarios.__file__)
---
+++ modulos.py/usuarios/impuestos/utilidades.py
@@ -0,0 +1,2 @@
+def pagar_impuestos():
+    print("pagando impuestos")
